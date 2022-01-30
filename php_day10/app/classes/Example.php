6b3c9805fb2e22beb28dae85cadc81dabaaa8cd5
Fardin Ahsan Shuvo <95983616+FardinShuvo@users.noreply.github.com>
2022-01-30 13:37:54 +0600

    Add files via upload

---
+++ php_day10/app/classes/Example.php
@@ -0,0 +1,182 @@
+<?php
+namespace App\classes;
+class Example
+{
+    public $name;
+    public $email;
+    public $mobile;
+    public $firstName;
+    public $lastName;
+    public $x;
+    public $y;
+    public $z;
+    public $data=[];
+
+    public function index()
+    {
+//        $this->firstName ='Rahim';
+//        $this->lastName = 'Khan';
+//        echo "full name is:". $this->firstName. ' '.$this->lastName;
+//       $this->x=10;
+//       $this->y=20;
+//       $this->z=30;
+        //logical operator
+
+
+//        echo ($this->x > $this->y) && ($this->y > $this->z);
+//        echo '<br/>...<br/>';
+//        echo ($this->x > $this->y) && ($this->y < $this->z);
+//        echo '<br/>...<br/>';
+//        echo ($this->x < $this->y) && ($this->y > $this->z);
+//        echo '<br/>...<br/>';
+//        echo ($this->x < $this->y) && ($this->y < $this->z);
+//        echo '<br/>...<br/>';
+//
+//        echo ($this->x > $this->y) || ($this->y > $this->z);
+//        echo '<br/>...<br/>';
+//        echo ($this->x > $this->y) || ($this->y < $this->z);
+//        echo '<br/>...<br/>';
+//        echo ($this->x < $this->y) || ($this->y > $this->z);
+//        echo '<br/>...<br/>';
+//        echo ($this->x < $this->y) || ($this->y < $this->z);
+//        echo '<br/>...<br/>';
+
+        //conditional operator
+//        echo $this->x > $this->y;
+//        echo '<br/>';
+//        echo $this->x < $this->y;
+//        echo '<br/>';
+//        echo $this->x >= $this->y;
+//        echo '<br/>';
+//        echo $this->x <= $this->y;
+//        echo '<br/>';
+//        echo $this->x == $this->y;
+//        echo '<br/>';
+//        echo $this->x != $this->y;
+//        echo '<br/>';
+
+        //assignment operator
+//       echo $this->x +=$this->y;
+//       echo '<br/>';
+//        echo $this->x -=$this->y;
+//        echo '<br/>';
+//        echo $this->x *=$this->y;
+//        echo '<br/>';
+//        echo $this->x /=$this->y;
+//        echo '<br/>';
+//        echo $this->x %=$this->y;
+//        echo '<br/>';
+//        echo $this->x .=$this->y;
+//        echo '<br/>';
+
+//
+//       echo $this->x;
+//       echo $this->x + $this->y;
+//       echo '<br/>';
+//       echo $this->x * $this->y;
+//        echo '<br/>';
+//        echo $this->x / $this->y;
+//        echo '<br/>';
+//        echo $this->x % $this->y;
+//
+
+//***********Statement
+        //Single line statement
+        //if
+//        $this->x=100;
+//        if($this->x>20)
+//        {
+//            echo $this->x;
+//        }
+
+//       **  if else
+//
+//        $this->x=10;
+//        if($this->x>20)
+//        {
+//            echo $this->x;
+//        }
+//        else{
+//            echo 'Hello World';
+//        }
+        //if else if
+//        $this->x=100;
+//        if($this->x > 200)
+//        {
+//            echo '200';
+//        }
+//        elseif ($this->x >200)
+//        {
+//            echo 'hello';
+//        }
+
+//        $this->x=100;
+//        if($this->x > 200)
+//        {
+//            echo 'hello';
+//        }
+//        elseif ($this->x >200)
+//        {
+//            echo 'hello';
+//        }
+//
+
+
+        //***switch
+
+        //$this->x=10;
+
+        //****switch case
+//        switch ($this->x)
+//        {
+//            case 10:
+//                echo 'hello';
+//                break;
+//            case 20:
+//                echo 'bangladesh';
+//                break;
+//            case 30:
+//                echo 'dhaka';
+//                break;
+//            default:
+//                echo 'bitm';
+//
+//        }
+
+        //for loop
+
+//        for($this->y=1; $this->y <=100;$this->y++)
+//        {
+//            echo $this->y. '';
+//        }
+//
+//
+//       while loop
+//
+//        $this->x=100;
+//        while ($this->x <110)
+//        {
+//            echo 'hello<br/>';
+//            $this->x++;
+//        }
+
+        //do while loop
+//            $this->x=100;
+//            do{
+//                echo 'hello<br/>';
+//                $this->x++;
+//            }
+//            while($this->x <110);
+
+        //*******for is loop(ARRAY)********************************************************
+        //start 0 theke
+
+//            $this->data=[10,20,30,'bitm',100.200,true];
+//            //echo $this->data[4];
+//            foreach ($this->data as $item)
+//            {
+//                echo $item
+//            }
+
+    }
+}
